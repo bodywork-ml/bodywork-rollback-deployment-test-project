948b9f1f4ee44fe69aac851c3c7989d57ac6e7fc
Marios Ioannides <Mariosi@hotmail.co.uk>
2021-10-07 10:28:03 +0100

    Update URL

--- stage_2/main.py
+++ stage_2/main.py
@@ -16,7 +16,7 @@ def predict():
 
 if __name__ == '__main__':
     try:
-        response = requests.get('http://bodywork-rollback-deployment-test-project--stage-3:5000/v2/predict')
+        response = requests.get('http://stage-3:5000/v2/predict')
     except requests.exceptions.ConnectionError:
         app.run(host='0.0.0.0', port=5000)
     finally:
